005faf0840a735a1b78a1d03d4a0d0153c7ab6b1
Hippolyte Pello <hpello@gmail.com>
2018-02-02 12:40:53 +0100

    Accesslog: Add JWT subject to access log when present.

--- middlewares/accesslog/logger_formatters.go
+++ middlewares/accesslog/logger_formatters.go
@@ -2,7 +2,11 @@ package accesslog
 
 import (
 	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -26,7 +30,7 @@ func (f *CommonLogFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 
 	_, err := fmt.Fprintf(b, "%s - %s [%s] \"%s %s %s\" %v %v %s %s %v %s %s %dms\n",
 		entry.Data[ClientHost],
-		entry.Data[ClientUsername],
+		extractJWTSubject(entry.Data["request_Authorization"], `-`),
 		timestamp,
 		entry.Data[RequestMethod],
 		entry.Data[RequestPath],
@@ -66,4 +70,64 @@ func quoted(s string, defaultValue string) string {
 		return defaultValue
 	}
 	return `"` + s + `"`
+}
+
+var authorizationRegexp = regexp.MustCompile(`^JWT [^\.]+\.([^\.]+)\.[^\.]+$`)
+var base64URLReplacer = strings.NewReplacer("-", "+", "_", "/")
+
+type jwtPayload struct {
+	Subject string `json:"sub"`
+}
+
+func extractJWTSubject(v interface{}, defaultValue string) string {
+	switch s := v.(type) {
+	case string:
+		subject := parseJWTSubject(s)
+		if len(subject) == 0 {
+			return defaultValue
+		}
+		return subject
+
+	default:
+		return defaultValue
+	}
+}
+
+func parseJWTSubject(s string) string {
+	result := authorizationRegexp.FindStringSubmatch(s)
+	if len(result) == 0 {
+		return ""
+	}
+
+	payloadBase64 := addBase64Padding(result[1])
+	if len(payloadBase64) == 0 {
+		return ""
+	}
+
+	payloadBase64Url := base64URLReplacer.Replace(payloadBase64)
+
+	payloadJSON, err := base64.StdEncoding.DecodeString(payloadBase64Url)
+	if err != nil {
+		return ""
+	}
+
+	payload := jwtPayload{}
+	err = json.Unmarshal([]byte(payloadJSON), &payload)
+	if err != nil {
+		return ""
+	}
+
+	return payload.Subject
+}
+
+func addBase64Padding(s string) string {
+	if len(s) % 4 == 0 {
+		return s
+	} else if len(s) % 4 == 2 {
+		return s + `==`
+	} else if len(s) % 4 == 3 {
+		return s + `=`
+	} else {
+		return ""
+	}
 }
